00256fb24a71ed521b07b909d205f1f74d15398e
Cody Lamson <tovarishfin@gmail.com>
2018-05-03 22:46:34 +0200

    mining added :D

--- block.go
+++ block.go
@@ -1,9 +1,6 @@
 package main
 
 import (
-	"bytes"
-	"crypto/sha256"
-	"strconv"
 	"time"
 )
 
@@ -13,27 +10,16 @@ type Block struct {
 	Data          []byte
 	PrevBlockHash []byte
 	Hash          []byte
-}
-
-// (args) before funcName() is a 'receiver'. is called as: Block.SetHash()
-// pointer is passed so that Hash can be set on block
-
-// SetHash sets the hash on a block using other data to create the hash
-func (b *Block) SetHash() {
-	// converts timestamp to base10 string and then converts to byte array
-	timestamp := []byte(strconv.FormatInt(b.Timestamp, 10))
-	// arrays are declared with {} in go... they are printed in [] format
-	headers := bytes.Join([][]byte{b.PrevBlockHash, b.Data, timestamp}, []byte{})
-	// create sha256 hash of headers which includes previous PrevBlockHash
-	hash := sha256.Sum256(headers)
-	// finally set the hash as a slice of array
-	b.Hash = hash[:]
+	Nonce         int
 }
 
 // NewBlock creates a new block based on previous block's hash
 func NewBlock(data string, prevBlockHash []byte) *Block {
-	block := &Block{time.Now().Unix(), []byte(data), prevBlockHash, []byte{}}
-	block.SetHash()
+	block := &Block{time.Now().Unix(), []byte(data), prevBlockHash, []byte{}, 0}
+	pow := NewProofOfWork(block)
+	nonce, hash := pow.Run()
+	block.Hash = hash[:]
+	block.Nonce = nonce
 	return block
 }
 
